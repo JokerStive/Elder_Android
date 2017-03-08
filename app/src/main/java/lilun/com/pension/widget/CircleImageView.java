e7d4938194ddbe7d019322a6bda9d9f9f60113c9
youke <1551332434@qq.com>
2017-03-08 16:31:11 +0800

    更新 ----居家服务（个人）

--- app/src/main/java/lilun/com/pension/widget/CircleImageView.java
+++ app/src/main/java/lilun/com/pension/widget/CircleImageView.java
@@ -20,8 +20,7 @@ import lilun.com.pension.R;
 
 
 /**
- *
- *圆形imageView
+ * 圆形imageView
  */
 
 public class CircleImageView extends ImageView {
@@ -244,8 +243,9 @@ public class CircleImageView extends ImageView {
     protected void onMeasure(int widthMeasureSpec, int heightMeasureSpec) {
         super.onMeasure(widthMeasureSpec, heightMeasureSpec);
 
-        int defWith=100;
-        int defHeight=100;
+        int defWith = 100;
+        int defHeight = 100;
+        int defSize;
         //获取宽高的测量模式
         int withSpecMode = MeasureSpec.getMode(widthMeasureSpec);
         int withSpecSize = MeasureSpec.getSize(widthMeasureSpec);
@@ -254,13 +254,19 @@ public class CircleImageView extends ImageView {
         int heightSpecSize = MeasureSpec.getSize(heightMeasureSpec);
 
         //当宽/高为wrap_content即对用AT_MOST的时候，设置一个默认值给系统
-        if (withSpecMode== MeasureSpec.AT_MOST && heightSpecMode== MeasureSpec.AT_MOST){
-            setMeasuredDimension(defWith,defHeight);
-        }else if (withSpecMode== MeasureSpec.AT_MOST){
-            setMeasuredDimension(defWith,heightSpecSize);
+        if (withSpecSize!=0 && heightSpecSize!=0){
+            defSize = withSpecSize>heightSpecSize?heightSpecSize:withSpecSize;
+            setMeasuredDimension(defSize, defSize);
         }else {
-            setMeasuredDimension(withSpecSize,defHeight);
+            setMeasuredDimension(defWith, defHeight);
         }
+//        if (withSpecMode == MeasureSpec.AT_MOST && heightSpecMode == MeasureSpec.AT_MOST) {
+//            setMeasuredDimension(defWith, defHeight);
+//        } else if (withSpecMode == MeasureSpec.AT_MOST) {
+//            setMeasuredDimension(defWith, heightSpecSize);
+//        } else {
+//            setMeasuredDimension(withSpecSize, defHeight);
+//        }
 
     }
 }
