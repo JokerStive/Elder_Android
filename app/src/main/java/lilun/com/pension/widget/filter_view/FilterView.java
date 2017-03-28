9ebc289a48dfe679265a96588416f40a161f13de
youke <1551332434@qq.com>
2017-03-28 14:00:31 +0800

    更新筛选栏

--- app/src/main/java/lilun/com/pension/widget/filter_view/FilterView.java
+++ app/src/main/java/lilun/com/pension/widget/filter_view/FilterView.java
@@ -1,7 +1,6 @@
 package lilun.com.pension.widget.filter_view;
 
 import android.content.Context;
-import android.graphics.Color;
 import android.util.AttributeSet;
 import android.util.Log;
 import android.view.MotionEvent;
@@ -22,7 +21,7 @@ import lilun.com.pension.R;
  *         create at 2017/3/27 10:21
  *         email : yk_developer@163.com
  */
-public class FilterView extends LinearLayout implements View.OnTouchListener {
+public class FilterView extends LinearLayout implements View.OnTouchListener, View.OnClickListener {
     private int tabIconDUp;
     private int tabIconDownChecked;
     private int mTitleColor;
@@ -31,13 +30,14 @@ public class FilterView extends LinearLayout implements View.OnTouchListener {
     private int tabIconDown;
     private Context context;
     private FrameLayout popViews;
-    private FrameLayout maskView;
-    private FrameLayout popViewsContainer;
+    //    private FrameLayout maskView;
+    private FrameLayout pop;
     private LinearLayout filterTabContainer;
     private int checkedTabPosition = -1;
     private int maxWidth = -1;
     private int maxHeight = -1;
     private FilterTabView mCurrentClickTabView;
+    private FrameLayout popContainer;
 
     public FilterView(Context context) {
         super(context, null);
@@ -76,13 +76,11 @@ public class FilterView extends LinearLayout implements View.OnTouchListener {
         filterTabContainer.setLayoutParams(params);
         addView(filterTabContainer);
 
-        popViewsContainer = new FrameLayout(context);
-        popViewsContainer.setBackgroundColor(Color.WHITE);
-//        popViewsContainer.setOrientation(VERTICAL);
+        pop = new FrameLayout(context);
         LinearLayout.LayoutParams lp = new LinearLayout.LayoutParams(ViewGroup.LayoutParams.MATCH_PARENT, ViewGroup.LayoutParams.MATCH_PARENT);
-//        lp.topMargin = 20;
-        popViewsContainer.setLayoutParams(lp);
-        addView(popViewsContainer);
+        pop.setLayoutParams(lp);
+        addView(pop);
+
 
 
     }
@@ -100,28 +98,23 @@ public class FilterView extends LinearLayout implements View.OnTouchListener {
 
         ViewGroup parent = (ViewGroup) contentView.getParent();
         parent.removeView(contentView);
-        popViewsContainer.addView(contentView);
+        pop.addView(contentView);
 
-
-        maskView = new FrameLayout(context);
-        maskView.setLayoutParams(new FrameLayout.LayoutParams(ViewGroup.LayoutParams.MATCH_PARENT, ViewGroup.LayoutParams.MATCH_PARENT));
-        maskView.setBackgroundColor(0xaa222222);
-        maskView.setOnTouchListener(this);
-        maskView.setVisibility(GONE);
-        popViewsContainer.addView(maskView);
+        popContainer = new FrameLayout(context);
+        popContainer.setLayoutParams(new LinearLayout.LayoutParams(ViewGroup.LayoutParams.MATCH_PARENT, ViewGroup.LayoutParams.MATCH_PARENT));
+        pop.addView(popContainer);
+        popContainer.setBackgroundColor(0x33222222);
+        popContainer.setVisibility(GONE);
+        popContainer.setOnTouchListener(this);
 
         popViews = new FrameLayout(context);
-        popViews.setVisibility(GONE);
         popViews.setLayoutParams(new ViewGroup.LayoutParams(ViewGroup.LayoutParams.MATCH_PARENT, ViewGroup.LayoutParams.WRAP_CONTENT));
-        popViewsContainer.addView(popViews);
+        popContainer.addView(popViews);
         for (int i = 0; i < pops.size(); i++) {
+            pops.get(i).setOnClickListener(this);
             popViews.addView(pops.get(i), i);
         }
 
-
-
-        Log.d("yk", "筛选项个数  = " + filterTabContainer.getChildCount());
-        Log.d("yk", "pop个数  = " + popViews.getChildCount());
 
     }
 
@@ -146,18 +139,16 @@ public class FilterView extends LinearLayout implements View.OnTouchListener {
                 } else {
                     //如果没有选中任何的tab
                     if (checkedTabPosition == -1) {
-                        popViews.setVisibility(VISIBLE);
-                        maskView.setVisibility(VISIBLE);
-                        popViews.setAnimation(AnimationUtils.loadAnimation(getContext(), R.anim.pop_container_in));
-
-                        maskView.setAnimation(AnimationUtils.loadAnimation(getContext(), R.anim.pop_container_in));
+                        popContainer.setVisibility(VISIBLE);
+                        popContainer.setAnimation(AnimationUtils.loadAnimation(getContext(), R.anim.pop_container_in));
                     }
+
                     View childAt = popViews.getChildAt(i);
                     childAt.setVisibility(VISIBLE);
-                    maxWidth = childAt.getMeasuredWidth();
-                    maxHeight = childAt.getMeasuredHeight();
-//                    Log.d("yk", "当前可见的view的宽高 = " + maxWidth + "--" + maxHeight);
-                    Log.d("yk", "pops 中位置为" + i + "的布局被显示");
+                    maxWidth = childAt.getRight();
+                    maxHeight = childAt.getBottom();
+                    Log.d("yk", "当前可见的view的宽高 = " + maxWidth + "--" + maxHeight);
+//                    Log.d("yk", "pops 中位置为" + i + "的布局被显示");
 
                     tabView.setTabStatus(true);
                     tabView.setColor(true);
@@ -167,7 +158,7 @@ public class FilterView extends LinearLayout implements View.OnTouchListener {
 
 
             } else {
-                Log.d("yk", "pops 中位置为" + i + "的布局被隐藏");
+//                Log.d("yk", "pops 中位置为" + i + "的布局被隐藏");
                 tabView.setTabStatus(false);
                 tabView.setColor(false);
                 View childAt = popViews.getChildAt(i);
@@ -188,11 +179,13 @@ public class FilterView extends LinearLayout implements View.OnTouchListener {
 
 
     private void closePop() {
-        popViews.setVisibility(View.GONE);
-        popViews.setAnimation(AnimationUtils.loadAnimation(getContext(), R.anim.pop_container_out));
-
-        maskView.setVisibility(GONE);
-        maskView.setAnimation(AnimationUtils.loadAnimation(getContext(), R.anim.pop_container_out));
+        popContainer.setVisibility(GONE);
+        popContainer.setAnimation(AnimationUtils.loadAnimation(getContext(), R.anim.pop_container_out));
+//        popViews.setVisibility(View.GONE);
+//        popViews.setAnimation(AnimationUtils.loadAnimation(getContext(), R.anim.pop_container_out));
+//
+//        maskView.setVisibility(GONE);
+//        maskView.setAnimation(AnimationUtils.loadAnimation(getContext(), R.anim.pop_container_out));
 
         checkedTabPosition = -1;
     }
@@ -201,14 +194,19 @@ public class FilterView extends LinearLayout implements View.OnTouchListener {
     public boolean onTouch(View v, MotionEvent event) {
         int action = event.getAction();
         if (action == MotionEvent.ACTION_DOWN) {
-            float rawX = event.getX();
-            float rawY = event.getY();
-            Log.d("yk", "点击位置的坐标 ==" + rawX + "---" + rawY);
-            if (!(rawX <= maxWidth && rawY <= maxHeight) && mCurrentClickTabView != null) {
-                switchPop(mCurrentClickTabView);
-
-            }
+            switchPop(mCurrentClickTabView);
+//            float rawX = event.getX();
+//            float rawY = event.getY();
+//            Log.d("yk", "点击位置的坐标 ==" + rawX + "---" + rawY);
+//            if (!(rawX <= maxWidth && rawY <= maxHeight) && mCurrentClickTabView != null) {
+//
+//            }
         }
         return true;
+    }
+
+    @Override
+    public void onClick(View v) {
+
     }
 }
