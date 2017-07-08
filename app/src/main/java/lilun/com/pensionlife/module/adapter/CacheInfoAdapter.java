f5f1d8a07b2d3a890e5bc46c076e48cd710787a8
zp <zp835228262@163.com>
2017-07-08 12:48:38 +0800

    #6563 查看发布通知的公告信息记录表，点击任意一条都不能进入详情页面. #6706 公告消息显示栏位的公告信息应该按照倒序排列，最新的应该排在最上面.

--- app/src/main/java/lilun/com/pensionlife/module/adapter/CacheInfoAdapter.java
+++ app/src/main/java/lilun/com/pensionlife/module/adapter/CacheInfoAdapter.java
@@ -1,5 +1,7 @@
 package lilun.com.pensionlife.module.adapter;
 
+import android.text.Html;
+
 import com.chad.library.adapter.base.BaseViewHolder;
 
 import java.util.List;
@@ -9,11 +11,10 @@ import lilun.com.pensionlife.base.QuickAdapter;
 import lilun.com.pensionlife.module.bean.CacheInfo;
 
 /**
-* 
-*@author yk
-*create at 2017/4/27 17:56
-*email : yk_developer@163.com
-*/
+ * @author yk
+ *         create at 2017/4/27 17:56
+ *         email : yk_developer@163.com
+ */
 public class CacheInfoAdapter extends QuickAdapter<CacheInfo> {
     public CacheInfoAdapter(List<CacheInfo> data) {
         super(R.layout.item_cache_info, data);
@@ -21,9 +22,15 @@ public class CacheInfoAdapter extends QuickAdapter<CacheInfo> {
 
     @Override
     protected void convert(BaseViewHolder helper, CacheInfo cacheInfo) {
-        helper.setText(R.id.first,cacheInfo.getFirst())
-                .setText(R.id.second,cacheInfo.getSecond())
-                .setText(R.id.third,cacheInfo.getThird())
-                .setText(R.id.fourth,cacheInfo.getFourth());
+        if (cacheInfo.getType() == 0) {
+            helper.setText(R.id.first, "创建人:" + cacheInfo.getFirst())
+                    .setText(R.id.second, "标题:" + cacheInfo.getSecond())
+                    .setText(R.id.third, "时间:" + cacheInfo.getThird())
+                    .setText(R.id.fourth, Html.fromHtml("<font color = '#F7631A'>点击进入详情</font>"));
+        } else
+            helper.setText(R.id.first, cacheInfo.getFirst())
+                    .setText(R.id.second, cacheInfo.getSecond())
+                    .setText(R.id.third, cacheInfo.getThird())
+                    .setText(R.id.fourth, cacheInfo.getFourth());
     }
 }
