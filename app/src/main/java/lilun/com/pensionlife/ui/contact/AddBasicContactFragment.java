25a9ebcbcd87ea0ffbe3487bd1e9b8c1399256cd
youke <1551332434@qq.com>
2017-08-09 17:00:34 +0800

    contact基础信息

--- app/src/main/java/lilun/com/pensionlife/ui/contact/AddBasicContactFragment.java
+++ app/src/main/java/lilun/com/pensionlife/ui/contact/AddBasicContactFragment.java
@@ -1,18 +1,38 @@
 package lilun.com.pensionlife.ui.contact;
 
 import android.view.LayoutInflater;
+import android.widget.EditText;
+import android.widget.TextView;
 
+import butterknife.Bind;
 import lilun.com.pensionlife.R;
+import lilun.com.pensionlife.app.User;
 import lilun.com.pensionlife.base.BaseFragment;
+import lilun.com.pensionlife.widget.NormalTitleBar;
+import lilun.com.pensionlife.widget.SwitchButton;
 
 /**
-*新增contact基础信息
-*@author yk
-*create at 2017/8/9 9:34
-*email : yk_developer@163.com
-*/
+ * 新增contact基础信息
+ *
+ * @author yk
+ *         create at 2017/8/9 9:34
+ *         email : yk_developer@163.com
+ */
 public class AddBasicContactFragment extends BaseFragment {
 
+
+    @Bind(R.id.titleBar)
+    NormalTitleBar titleBar;
+    @Bind(R.id.et_contact_name)
+    EditText etContactName;
+    @Bind(R.id.et_contact_mobile)
+    EditText etContactMobile;
+    @Bind(R.id.tv_choose_address)
+    TextView tvChooseAddress;
+    @Bind(R.id.et_contact_address)
+    EditText etContactAddress;
+    @Bind(R.id.sb_set_default)
+    SwitchButton sbSetDefault;
 
     public static AddBasicContactFragment newInstance() {
         AddBasicContactFragment fragment = new AddBasicContactFragment();
@@ -21,6 +41,7 @@ public class AddBasicContactFragment extends BaseFragment {
 //        fragment.setArguments(args);
         return fragment;
     }
+
     @Override
     protected void initPresenter() {
 
@@ -33,6 +54,8 @@ public class AddBasicContactFragment extends BaseFragment {
 
     @Override
     protected void initView(LayoutInflater inflater) {
-
+        titleBar.setOnBackClickListener(this::pop);
+        etContactMobile.setText(User.getMobile());
     }
+
 }
