00e3c2ffce42870f5c00518e02b07b7ff7217fbe
zp <zp835228262@163.com>
2017-09-21 15:30:39 +0800

    版本1.1.9小改             1.清除多余的import代码

--- app/src/main/java/lilun/com/pensionlife/ui/home/HomeFragment.java
+++ app/src/main/java/lilun/com/pensionlife/ui/home/HomeFragment.java
@@ -12,7 +12,6 @@ import android.view.View;
 import android.widget.ImageView;
 import android.widget.TextView;
 
-import com.antfortune.freeline.util.AppUtils;
 import com.orhanobut.logger.Logger;
 
 import org.greenrobot.eventbus.Subscribe;
