657875d760411b97b4473c01605ea9f38cd40aa4
zp <zp835228262@163.com>
2018-03-28 13:09:27 +0800

    版本1.4.1: 登录界面添加动画；

--- app/src/main/java/lilun/com/pensionlife/ui/welcome/LoginActivity.java
+++ app/src/main/java/lilun/com/pensionlife/ui/welcome/LoginActivity.java
@@ -1,11 +1,21 @@
 package lilun.com.pensionlife.ui.welcome;
 
+import android.animation.AnimatorSet;
+import android.animation.ObjectAnimator;
 import android.content.Intent;
 import android.graphics.drawable.Drawable;
 import android.support.v4.content.ContextCompat;
+import android.support.v4.widget.NestedScrollView;
+import android.util.Log;
+import android.view.View;
+import android.view.animation.LinearInterpolator;
 import android.view.inputmethod.EditorInfo;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
+import android.widget.RelativeLayout;
+import android.widget.ScrollView;
 import android.widget.TextView;
 
 import butterknife.Bind;
@@ -34,7 +44,10 @@ public class LoginActivity extends BaseActivity<LoginContract.Presenter> impleme
 
     Account account;
 
-
+    @Bind(R.id.iv_logo)
+    ImageView ivLogo;
+    @Bind(R.id.ll_container)
+    LinearLayout llContainer;
     @Bind(R.id.tv_forget_password)
     TextView tvForgetPassword;
     @Bind(R.id.et_mobile)
@@ -51,9 +64,14 @@ public class LoginActivity extends BaseActivity<LoginContract.Presenter> impleme
 
     @Bind(R.id.tv_new_account)
     TextView tvNewAccount;
+    @Bind(R.id.nsv_scrollview)
+    NestedScrollView nsvView;
+    @Bind(R.id.rl_container)
+    RelativeLayout rlContainer;
 
     private boolean autologin;
     private boolean isShow = false;
+    private int keyHeight; //屏幕1/3
 
     @Override
     protected void onActivityResult(int requestCode, int resultCode, Intent data) {
@@ -80,6 +98,9 @@ public class LoginActivity extends BaseActivity<LoginContract.Presenter> impleme
     protected void initData() {
         autologin = getIntent().getBooleanExtra("autologin", false);
         account = (Account) getIntent().getSerializableExtra("account");
+        //弹起高度为屏幕高度的1/3
+        keyHeight = this.getResources().getDisplayMetrics().heightPixels / 3; //获取屏幕高度
+
     }
 
     @Override
@@ -109,6 +130,34 @@ public class LoginActivity extends BaseActivity<LoginContract.Presenter> impleme
             login();
         }
         etPassword.setOnKeyListener(editOnKeyListener);
+        nsvView.addOnLayoutChangeListener(((v, left, top, right, bottom, oldLeft, oldTop, oldRight, oldBottom) -> {
+             /* old是改变前的左上右下坐标点值，没有old的是改变后的左上右下坐标点值
+              现在认为只要控件将Activity向上推的高度超过了1/3屏幕高，就认为软键盘弹起*/
+            if (oldBottom != 0 && bottom != 0 && (oldBottom - bottom > keyHeight)) {
+                Log.e("wenzhihao", "up------>" + (oldBottom - bottom) + " " + rlContainer.getHeight());
+                int dist = llContainer.getBottom() - bottom - rlContainer.getHeight();
+                if (dist > 0) {
+                    ObjectAnimator mAnimatorTranslateY = ObjectAnimator.ofFloat(llContainer, "translationY", 0.0f, -dist);
+                    mAnimatorTranslateY.setDuration(300);
+                    mAnimatorTranslateY.setInterpolator(new LinearInterpolator());
+                    mAnimatorTranslateY.start();
+                    zoomIn(ivLogo, 0.6f, dist);
+                }
+
+
+            } else if (oldBottom != 0 && bottom != 0 && (bottom - oldBottom > keyHeight)) {
+                Log.e("wenzhihao", "down------>" + (bottom - oldBottom));
+                if ((llContainer.getBottom() - oldBottom) > 0) {
+                    ObjectAnimator mAnimatorTranslateY = ObjectAnimator.ofFloat(llContainer, "translationY", llContainer.getTranslationY(), 0);
+                    mAnimatorTranslateY.setDuration(300);
+                    mAnimatorTranslateY.setInterpolator(new LinearInterpolator());
+                    mAnimatorTranslateY.start();
+                    //键盘收回后，logo恢复原来大小，位置同样回到初始位置
+                    zoomOut(ivLogo, 0.6f);
+                }
+
+            }
+        }));
     }
 
 
@@ -167,6 +216,45 @@ public class LoginActivity extends BaseActivity<LoginContract.Presenter> impleme
     @Override
     public void activateAccount(String mobile) {
         ActivateAccountManager.newInstance().activate(this, mobile);
+    }
+
+    /**
+     * 缩小
+     *
+     * @param view
+     */
+    public static void zoomIn(final View view, float scale, float dist) {
+        view.setPivotY(view.getHeight());
+        view.setPivotX(view.getWidth() / 2);
+        AnimatorSet mAnimatorSet = new AnimatorSet();
+        ObjectAnimator mAnimatorScaleX = ObjectAnimator.ofFloat(view, "scaleX", 1.0f, scale);
+        ObjectAnimator mAnimatorScaleY = ObjectAnimator.ofFloat(view, "scaleY", 1.0f, scale);
+        ObjectAnimator mAnimatorTranslateY = ObjectAnimator.ofFloat(view, "translationY", 0.0f, -dist);
+
+        mAnimatorSet.play(mAnimatorTranslateY).with(mAnimatorScaleX);
+        mAnimatorSet.play(mAnimatorScaleX).with(mAnimatorScaleY);
+        mAnimatorSet.setDuration(300);
+        mAnimatorSet.start();
+    }
+
+    /**
+     * f放大
+     *
+     * @param view
+     */
+    public static void zoomOut(final View view, float scale) {
+        view.setPivotY(view.getHeight());
+        view.setPivotX(view.getWidth() / 2);
+        AnimatorSet mAnimatorSet = new AnimatorSet();
+
+        ObjectAnimator mAnimatorScaleX = ObjectAnimator.ofFloat(view, "scaleX", scale, 1.0f);
+        ObjectAnimator mAnimatorScaleY = ObjectAnimator.ofFloat(view, "scaleY", scale, 1.0f);
+        ObjectAnimator mAnimatorTranslateY = ObjectAnimator.ofFloat(view, "translationY", view.getTranslationY(), 0);
+
+        mAnimatorSet.play(mAnimatorTranslateY).with(mAnimatorScaleX);
+        mAnimatorSet.play(mAnimatorScaleX).with(mAnimatorScaleY);
+        mAnimatorSet.setDuration(300);
+        mAnimatorSet.start();
     }
 }
 
